497b4a7d2243b7e4997d498788f9017f47bd75fb
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-12-16 21:45:42 +0100

    add basic qr animation

--- sender/sender/main.py
+++ sender/sender/main.py
@@ -1,10 +1,46 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.core.image import Image as CoreImage
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+from kivy.config import Config
+import uuid
+import qrcode
+import io
+
+Config.set("modules", "monitor", "")
 
 
 class MyApp(App):
     def build(self):
-        return Label(text="Hello world")
+        self.title = "QR Sender"
+
+        self.display = True
+        self.data = [uuid.uuid4() for _ in range(100)]
+        self.cursor = 0
+
+        self.buffer = io.BytesIO()
+        self.image = Image(source="")
+
+        layout = BoxLayout()
+        layout.add_widget(self.image)
+
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+        return layout
+
+    def update(self, dt):
+        self.buffer.truncate(0)
+        self.buffer.seek(0)
+
+        qr = qrcode.make(self.data[self.cursor])
+        qr.save(self.buffer, ext="png")
+
+        self.cursor = (self.cursor + 1) % len(self.data)
+
+        self.buffer.seek(0)
+        image = io.BytesIO(self.buffer.read())
+        self.image.texture = CoreImage(image, ext="png").texture
 
 
 def run():
