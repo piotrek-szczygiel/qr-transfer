4837d98c213469bff1f36845471deee398f4a831
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-12-17 02:07:26 +0100

    add file dialog

--- sender/sender/encoder.py
+++ sender/sender/encoder.py
@@ -1,4 +1,5 @@
 import io
+import math
 
 import numpy as np
 import segno
@@ -6,7 +7,7 @@ from kivy.core.image import Image as CoreImage
 from PIL import Image
 
 
-def encode_frame(a, b, c, scale=1):
+def encode_frame(a, b, c, frame_size):
     assert len(a) == len(b) == len(c)
 
     data = [segno.make(x) for x in (a, b, c)]
@@ -16,15 +17,14 @@ def encode_frame(a, b, c, scale=1):
     for i in range(3):
         rgb[..., i] = np.array(data[i].matrix) * 255
 
+    scale = math.ceil(frame_size / size)
     img = Image.fromarray(rgb).resize((size * scale, size * scale))
+
     buffer = io.BytesIO()
     img.save(buffer, format="png")
     buffer.seek(0)
     return CoreImage(io.BytesIO(buffer.read()), ext="png").texture
 
 
-def generate_frames(data):
-    frames = []
-    for i in range(0, len(data), 3):
-        frames.append(encode_frame(*data[i : i + 3], scale=10))
-    return frames
+def generate_frames(data, frame_size=400):
+    return [encode_frame(*data[i : i + 3], frame_size) for i in range(0, len(data), 3)]
--- sender/sender/main.py
+++ sender/sender/main.py
@@ -1,9 +1,14 @@
+import os
 import uuid
 
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.uix.popup import Popup
 
 from .encoder import generate_frames
 
@@ -12,11 +17,6 @@ class Ui(BoxLayout):
     def __init__(self, **kwargs):
         super(Ui, self).__init__(**kwargs)
 
-        self.frames = generate_frames([str(uuid.uuid4()) for _ in range(99)])
-        self.cursor = 0
-
-        Clock.schedule_interval(self.update, 1.0 / 30.0)
-
     def update(self, dt):
         self.ids.frame_a.texture = self.frames[self.cursor]
         self.ids.frame_b.texture = self.frames[self.cursor]
@@ -24,15 +24,43 @@ class Ui(BoxLayout):
         self.ids.frame_d.texture = self.frames[self.cursor]
         self.cursor = (self.cursor + 1) % len(self.frames)
 
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            data = stream.read()
+            self.frames = generate_frames([data for _ in range(3)])
+            self.cursor = 0
+            self.ids.start.disabled = False
+
+        self.dismiss_popup()
+
+    def start(self):
+        Clock.schedule_interval(self.update, 1.0 / 30.0)
+
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
 
 class SenderApp(App):
     from kivy.config import Config
 
-    Config.set("graphics", "width", 800)
+    Config.set("graphics", "width", 730)
     Config.set("graphics", "height", 800)
     Config.set("modules", "monitor", "")
 
     def build(self):
+        from kivy.core.window import Window
+
+        Window.clearcolor = (1, 1, 1, 1)
         return Ui()
 
 
